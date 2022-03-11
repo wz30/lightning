a3fd91b449375e699a8a0859595cd20e0d8bb128
wei zhang <wz146@duke.edu>
2022-03-11 18:29:59 +0000

    add bash file to complie file

---
+++ dist-src/fcli.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -o fcli fake_client.cc /usr/local/lib/libredis++.a /usr/local/lib/libhiredis.a -pthread
---
+++ dist-src/rcli.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -o rcli redis_client.cc /usr/local/lib/libredis++.a /usr/local/lib/libhiredis.a -pthread
