92d0ce8d9c3d69b147d6ca86f35cc8c26ad68180
wz30 <wz30@iu.edu>
2022-03-13 21:56:43 -0400

    add join

--- dist-src/thr_dist_mp_bench.cc
+++ dist-src/thr_dist_mp_bench.cc
@@ -93,6 +93,7 @@ int main(int argc, char *argv[])
         auto start = std::chrono::high_resolution_clock::now();
         for(int i = 2; i<num_tests; i++) {
             std::thread t1(send_cluster, sock, i);
+            t1.detach();
             // send_cluster(sock, i);
         }
         auto end = std::chrono::high_resolution_clock::now();
